1a571c9e4ebd9023c90832ad6d1deef0756a27a2
Bourque <Alex.Bourque@teluq.ca>
2024-05-10 13:45:06 -0400

    ajout code v1

---
+++ src/MoodleInstallerPlugin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace teluq;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class MoodleInstallerPlugin implements PluginInterface {
+
+    /**
+     *
+     */
+    public function activate(Composer $composer, IOInterface $io) {
+        $installer = new MoodleInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+
+    /**
+     *
+     */
+    public function uninstall(Composer $composer, IOInterface $io) {
+    }
+
+    /**
+     *
+     */
+    public function deactivate(Composer $composer, IOInterface $io) {
+        $installer = new MoodleInstaller($io, $composer);
+        $composer->getInstallationManager()->removeInstaller($installer);
+    }
+}
